764ab7587ded2db19dbf69a477f80316996e2105
UmarAziz <umar10022004@gmail.com>
2025-09-17 20:51:19 +0500

    feat: authentication

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,15 @@
+class ApplicationController < ActionController::Base
+  # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
+  allow_browser versions: :modern
+  
+  # Devise configuration
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:email, :password, :name])
+    devise_parameter_sanitizer.permit(:sign_in, keys: [:email, :password])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:email, :password, :name])
+  end
+end
---
+++ app/models/user.rb
@@ -0,0 +1,14 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  has_one_attached :avatar   
+  
+
+  # validates :avatar,
+  # attached: true,
+  # content_type: ['image/png', 'image/jpeg', 'image/jpg'],
+  # size: { less_than: 5.megabytes }
+end
